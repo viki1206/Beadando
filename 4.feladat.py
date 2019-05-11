76b46a09cd7cbc868c298386462c122ac88fa98e
viki1206 <viki120698@gmail.com>
2019-05-11 12:38:04 +0200

    asd

--- 4.feladat.py
+++ 4.feladat.py
@@ -1,6 +1,10 @@
-def alvas(ev,ho,nap):
-    honap=[31,28,31,30,31,30,31,31,30,31,30,31]
-    if(ev%4==0):
-        honap[1]+=1
-    return honap
-alvas(1975,5,1)
+def alvas(ev, ho, nap):
+    honap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if(ev%4== 0):
+        honap[1] += 1
+    if ho>12 or ho<1:
+        return print("Nincs ilyen hónap")
+
+
+
+alvas(2000, 4, 5)
