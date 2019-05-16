513178bf74edd3adcf5915d6d6a5e2b165a36474
viki1206 <37153495+viki1206@users.noreply.github.com>
2019-05-16 21:57:43 +0200

    vége

--- 5.feladat.py
+++ 5.feladat.py
@@ -1,28 +1,17 @@
-def sorozat(a):
-    h = []
-    e = []
-    num = 0
-    db = 0
-    for j in range(0, len(a) - 1):
-        h.append(a[j])
-        for k in range(j + 1, len(a) - 1):
-            for i in range(k, len(a)):
-                if h[db] < a[i]:
-                    h.append(a[i])
-                    db += 1
-            if num < len(h):
-                e = h
-                num = len(h)
-                h = [a[j]]
-                db = 0
+def sorozat(a): 
+    h = [] 
+    e = [] 
+    for j in range(0, len(a) - 1): 
+        h.append(a[j]) 
+        for k in range(j + 1, len(a) - 1): 
+            for i in range(k, len(a)): 
+                if h[len(h)-1] < a[i]: 
+                    h.append(a[i]) 
+            if len(e)< len(h):
+                e = h 
+                h = [a[j]] 
             else:
-                h = [a[j]]
-                db = 0
-    print(num, ': ', e)
-
+                h = [a[j]] 
+    print(len(e), ': ', e)
 sor = [1, 7, 3, 5, 4, 2]
 sorozat(sor)
-
-
-
-
